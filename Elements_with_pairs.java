7e2b3003b4a2687a42e48cc19202d7edb0398421
21MH5A0230 <110669057+21MH5A0230@users.noreply.github.com>
2022-09-23 21:26:22 +0530

    import java.util.Scanner; class main{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[];         n=sc.nextInt();         x=new int[n];         for(int i=0;i<n;i++)         x[i]=sc.nextInt();         for(int i=0;i<n;i++)         System.out.print(x[i]+" ");         if(n%2!=0)System.out.print("0");     } }

---
+++ Elements_with_pairs.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class main{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[];
+        n=sc.nextInt();
+        x=new int[n];
+        for(int i=0;i<n;i++)
+        x[i]=sc.nextInt();
+        for(int i=0;i<n;i++)
+        System.out.print(x[i]+" ");
+        if(n%2!=0)System.out.print("0");
+    }
+}
