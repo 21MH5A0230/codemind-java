dff620051feec59e944a6c8372cb60495b015382
21MH5A0230 <110669057+21MH5A0230@users.noreply.github.com>
2022-10-14 18:24:33 +0530

    import java.util.Scanner; class navya{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         int c=(n-3);         int m=c*n/2;         System.out.println(m);     } }

---
+++ Diagonals_of_polygon.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class navya{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        int c=(n-3);
+        int m=c*n/2;
+        System.out.println(m);
+    }
+}
